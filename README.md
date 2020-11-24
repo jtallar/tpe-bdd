1a3ae18e56065bfadd4bcd3874633761db639f63
Julian Tallar <jtallar@itba.edu.ar>
2020-11-24 14:14:08 -0300

    Added rating generation when populating

--- README.md
+++ README.md
@@ -9,10 +9,12 @@
 - npm install random-words
 
 # Populate MongoDB
-`node populate.js file.csv linesToProcess [DROP]`
+`node populate.js file.csv linesToProcess rateCount [DROP]`
 
-Where file.csv is a csv file with headers YearOfBirth,Name,Sex,Number; linesToProcess
-is a number to set maxLines to process; and `[DROP]` indicates if the collection 
-should be dropped before inserting.
+Where 
+- `file.csv` is a csv file with headers YearOfBirth,Name,Sex,Number 
+- `linesToProcess` is a number (> 0) to set maxLines to process
+- `rateCount` is a number (>= 0) to set the number of rates to generate between added users
+- `[DROP]` indicates if the collection should be dropped before inserting.
 
-Eg: `node populate.js babyNamesUSYOB-full.csv 20 DROP`
+Eg: `node populateMongo.js babyNamesUSYOB-full.csv 10 20 DROP`
--- populateMongo.js
+++ populateMongo.js
@@ -2,6 +2,7 @@ const { MongoClient } = require("mongodb");
 
 const mongoFun = require('./mongoQueries');
 const objectFun = require('./objects');
+const utilsFun = require('./utils');
 
 const csv = require('csv-parser');
 const fs = require('fs');
@@ -11,16 +12,35 @@ const randomWords = require('random-words');
 const uri = "mongodb://localhost:27017";
 const mClient = new MongoClient(uri, { useUnifiedTopology: true });
 
-async function populateMongo(shouldDrop) {
+async function populateMongo(users, shouldDrop, rateCount) {
     try {
         await mClient.connect();
         const database = mClient.db('nosedive');
         const collection = database.collection('users');
 
+        // Drop all documents if requested
         if (shouldDrop) {
-            await mongoFun.dropAll(collection);
+            try {
+                await mongoFun.dropAll(collection);
+            } catch (err) {
+                console.log('Nothing to drop')
+            }
         }
+        // Insert users into collection
         await mongoFun.insertManyUsers(collection, users);
+        // Generate rateCount random ratings
+        var i;
+        for (i = 0; i < rateCount; i++) {
+            var index0 = utilsFun.randomBetween(0, users.length);
+            var index1 = utilsFun.randomBetween(0, users.length);
+            if (index1 == index0) index1 = (index1 + 1) % users.length;
+            var score = utilsFun.randomBetween(1, 6); // Rating [1,5]
+
+            // TODO: Generate random locations
+            var rating = objectFun.newRatingJson(score, users[index0]._id, users[index1]._id, 0, 0);
+            await mongoFun.rateUser(collection, rating);
+        }
+
     } finally {
         // Ensures that the client will close when you finish/error
         await mClient.close();
@@ -34,9 +54,9 @@ var streamOptions = csv({
 });
 
 // Recibir el nombre por parametro
-var myArgs = process.argv.slice(2);
-if (myArgs.length < 2) {
-    console.log('Please run with `node populate.js file.csv linesToProcess [DROP]`');
+var myArgs = process.argv.slice(2); // Elimino node y fileName
+if (myArgs.length < 3) {
+    console.log('Please run with `node populate.js file.csv linesToProcess rateCount [DROP]`');
     return;
 }
 // const csvFilename = 'babyNamesUSYOB-full.csv';
@@ -44,12 +64,18 @@ const csvFilename = myArgs[0];
 
 var maxLines = parseInt(myArgs[1]);
 if (isNaN(maxLines) || maxLines <= 0) {
-    console.log('linesToProcess must be a number higher than 0');
+    console.log('linesToProcess must be a number greater than 0');
+    return;
+}
+
+var rateCount = parseInt(myArgs[2]);
+if (isNaN(rateCount) || rateCount < 0) {
+    console.log('linesToProcess must be a number greater or equal than 0');
     return;
 }
 
 var shouldDrop = false;
-if (myArgs[2] === 'DROP') {
+if (myArgs[3] === 'DROP') {
     shouldDrop = true;
 }
 
@@ -81,8 +107,7 @@ var stream = fs.createReadStream(csvFilename);
     })
     .on('unpipe', function() { // Uso unpipe porque cubre ambos casos (por fin de archivo y por limite maxLines)
         console.log(`Total lines processed: ${processedLines}`);
-        console.log(users.length)
-        
+
         // Connect to MongoDB and insert users in MongoDB
-        populateMongo(shouldDrop);
+        populateMongo(users, shouldDrop, rateCount);
     });
